dd6be2ce386eb53c8be5aa4ef5325b485070679d
linpoi387 <167059057+linpoi387@users.noreply.github.com>
2024-12-16 13:47:06 +0800

    Update api_scheduler.py

--- api_scheduler.py
+++ api_scheduler.py
@@ -13,8 +13,8 @@ scheduler = BackgroundScheduler(timezone='Asia/Taipei')
 scheduler.add_job(
     crawler,
     trigger = 'cron',
-    hour = 12,
-    # minute = 6,
+    hour = 13,
+    minute = 50,
     day_of_week = '*',
     misfire_grace_time = 60  # 寬限時間為 60 秒      
     )
