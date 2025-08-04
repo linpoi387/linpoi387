0deae3536389023a1bfc24762f38260bc60833ed
linpoi387 <167059057+linpoi387@users.noreply.github.com>
2025-08-04 14:06:28 +0800

    Update api_scheduler.py

--- api_scheduler.py
+++ api_scheduler.py
@@ -1,31 +1,28 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.triggers.date import DateTrigger
+# from apscheduler.schedulers.background import BackgroundScheduler
 from loguru import logger
 from cathay.cathaybk import crawler_cathaybk
 from esun.esun_crawler import crawler_esun
 import time
-import datetime
 
 
 def crawler():
     crawler_cathaybk()
     crawler_esun()
 
-scheduler = BackgroundScheduler(timezone='Asia/Taipei')
-scheduler.add_job(
-    crawler,
-    trigger = 'cron',
-    hour = 12,
-    # minute = 55,
-    day_of_week = '*',
-    misfire_grace_time = 600,  # 寬限時間為 600 秒  
-    trigger = DateTrigger(run_date=datetime.datetime.now() + datetime.timedelta(seconds=600)),
-    )
+# scheduler = BackgroundScheduler(timezone='Asia/Taipei')
+# scheduler.add_job(
+#     crawler,
+#     trigger = 'cron',
+#     hour = 12,
+#     # minute = 55,
+#     day_of_week = '*',
+#     misfire_grace_time = 600,  # 寬限時間為 600 秒  
+#     )
 
 logger.info('sent_crawler_task')
-scheduler.start()
+# scheduler.start()
+crawler()
 
-
-time.sleep(3600)
+time.sleep(3)
    
     
